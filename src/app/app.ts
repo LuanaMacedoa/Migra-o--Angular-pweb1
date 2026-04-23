1ee261c801a0bc9b55454e0c5d2e3b1782044314
Luana Gabriella <luanagabriellapsm@gmail.com>
2026-04-22 23:52:52 -0300

    Refactor

--- src/app/app.ts
+++ src/app/app.ts
@@ -12,53 +12,42 @@ import { TaskListComponent } from './components/task-list/task-list';
   imports: [CommonModule, FormsModule, TaskFormComponent, TaskListComponent]
 })
 export class AppComponent {
-  // Controle do estado do modal (Signal para melhor performance)
   isModalOpen = signal(false);
   
-  // Armazena a tarefa que está sendo editada (null se for uma nova tarefa)
   taskBeingEdited = signal<Task | null>(null);
 
   constructor(public taskService: TaskService) {}
 
-  /**
-   * Abre o modal para criar uma nova tarefa
-   */
+  
   openModal() {
     this.taskBeingEdited.set(null);
     this.isModalOpen.set(true);
   }
 
-  /**
-   * Abre o modal preenchido para edição
-   * Chamado pelo evento (editTask) do TaskListComponent
-   */
+  
   openEditModal(task: Task) {
     this.taskBeingEdited.set(task);
     this.isModalOpen.set(true);
   }
 
-  /**
-   * Fecha o modal e limpa o estado de edição
-   */
+  
   closeModal() {
     this.isModalOpen.set(false);
     this.taskBeingEdited.set(null);
   }
 
-  /**
-   * Recebe os dados do TaskFormComponent e decide se salva ou atualiza
-   */
+ 
   handleSave(taskData: any) {
     const currentTask = this.taskBeingEdited();
 
     if (currentTask) {
-      // Se existe uma tarefa sendo editada, atualizamos ela mantendo o ID e Status
+      
       this.taskService.updateTask({
         ...currentTask,
         ...taskData
       });
     } else {
-      // Se não, adicionamos como uma nova tarefa
+      
       this.taskService.addTask(taskData);
     }
 
